7127684cc9d804288deef0b00a6422f2755e2527
Rob Clarke <kumo@kumo.it>
2014-11-01 14:24:45 +0100

    a simple varese news tracker

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,15 +1,15 @@
 var keystone = require('keystone');
 
+var Student = keystone.list('Student');
+
 exports = module.exports = function(req, res) {
-	
+
 	var view = new keystone.View(req, res),
 		locals = res.locals;
-	
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
-	locals.section = 'home';
-	
-	// Render the view
-	view.render('index');
-	
-};
+
+	locals.section = 'members';
+
+	view.query('students', Student.model.find().sort('name.last'));
+
+	view.render('students');
+}
